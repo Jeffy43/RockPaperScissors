2d9eb7e0074f12d8f9d68499cd62292eb3319946
Jeffy Pun <jeffyp@nycstudents.net>
2023-10-31 08:40:13 -0400

    Update

--- src/Game.java
+++ src/Game.java
@@ -3,38 +3,82 @@ public class Game {
     private String userInput;
     private String computerMove;
 
+
+
+
     public Game(String choice){
         userInput = choice;
     }
 
+
+
+
     public String computerMove(){
-        int a = (int)Math.random() * 3;
+        int a = (int)(Math.random() * 5);
         String b;
         if (a == 0){
             b = "rock";
         } else if (a == 1){
             b = "paper";
+        }else if (a == 2){
+            b = "scissors";
+        }else if(a == 3){
+            b = "spock";
         }else{
-            b = "Scissors";
+            b = "lizard";
         }
         return b;
     }
 
-    public String decision(String computerChoice){
+
+
+
+    public String decision(String computerChoice) {
         String a;
-        if (userInput.equals(computerChoice)){
+        if (userInput.equals(computerChoice)) {
             a = "It's a tie";
-        }else if(userInput.equals("rock") && computerChoice.equals("paper")){
+        } else if (userInput.equals("rock") && computerChoice.equals("paper")) {
             a = "You lose";
-        }else if(userInput.equals("rock") && computerChoice.equals("scissors")){
+        } else if (userInput.equals("rock") && computerChoice.equals("scissors")) {
             a = "You win";
-        }else if(userInput.equals("paper") && computerChoice.equals("rock")){
+        } else if (userInput.equals("rock") && computerChoice.equals("spock")) {
+            a = "You lose";
+        } else if (userInput.equals("rock") && computerChoice.equals("lizard")) {
             a = "You win";
-        }else if(userInput.equals("paper") && computerChoice.equals("scissor")){
+        } else if (userInput.equals("paper") && computerChoice.equals("rock")) {
+            a = "You win";
+        } else if (userInput.equals("paper") && computerChoice.equals("scissor")) {
             a = "You lose";
-        }else if(userInput.equals("scissor") && computerChoice.equals("rock")){
+        } else if (userInput.equals("paper") && computerChoice.equals("spock")) {
             a = "You lose";
-        }else{a = "You win";
+        } else if (userInput.equals("paper") && computerChoice.equals("lizard")) {
+            a = "You lose";
+        } else if (userInput.equals("scissor") && computerChoice.equals("rock")) {
+            a = "You lose";
+        } else if (userInput.equals("scissor") && computerChoice.equals("paper")) {
+            a = "You win";
+        } else if (userInput.equals("scissor") && computerChoice.equals("spock")) {
+            a = "You lose";
+        } else if (userInput.equals("scissor") && computerChoice.equals("lizard")) {
+            a = "You win";
+        } else if (userInput.equals("scissor") && computerChoice.equals("paper")) {
+            a = "You win";
+        } else if (userInput.equals("lizard") && computerChoice.equals("paper")) {
+            a = "You win";
+        } else if (userInput.equals("lizard") && computerChoice.equals("rock")) {
+            a = "You lose";
+        } else if (userInput.equals("lizard") && computerChoice.equals("spock")) {
+            a = "You win";
+        } else if (userInput.equals("lizard") && computerChoice.equals("scissor")) {
+            a = "You lose";
+        } else if (userInput.equals("spock") && computerChoice.equals("paper")) {
+            a = "You lose";
+        } else if (userInput.equals("spock") && computerChoice.equals("rock")) {
+            a = "You win";
+        } else if (userInput.equals("spock") && computerChoice.equals("lizard")) {
+            a = "You lose";
+        } else{
+            a = "You win";
         }
         return a;
     }
--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 public class GameRunner{
-    Scanner scan = new Scanner(System.in);
-    public void main(String[] args){
-        System.out.print("Welcome to Rock, Paper, Scissors!\nRock beats Sissorcs. Sissors beat paper. Paper beats Rock." +
-                "The computer has chosen. What's your choice? (rock, paper or scissors)\nYour choice: ");
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Welcome to Rock, Paper, Scissors, Spock, Lizard!\nRock beats Scissors and Lizards. Scissors beat Paper and Lizards. Paper beats Rock and Spock. Lizard beats Paper and Spock. Spock beats Rock and Scissors." +
+                "The computer has chosen. What's your choice? (rock, paper, scissors, spock, or lizard)\nYour choice: ");
         String choice = scan.nextLine();
         Game game = new Game(choice);
         String computerChoice = game.computerMove();
-        System.out.print("Computer chose: " + computerChoice);
+        System.out.println("Computer chose: " + computerChoice);
         System.out.print(game.decision(computerChoice));
     }
 }
+
+
